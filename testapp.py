79def77bcbb3beccec4f6abb02c2dacaf3b2b995
duncanhs <84508932+duncanhs@users.noreply.github.com>
2022-11-25 16:57:45 +0100

    Update testapp.py

--- testapp.py
+++ testapp.py
@@ -17,7 +17,9 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 
 app.wsgi_app = ProxyFix(
     app.wsgi_app,
-    #x_for=1, x_proto=1, x_host=1, x_prefix=1
+    x_for=1,
+    #x_proto=1, x_host=1, 
+    x_prefix=1
 )
 
 # The route() function of the Flask class is a decorator,
