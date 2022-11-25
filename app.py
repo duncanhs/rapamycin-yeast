66f9622e3e07aeeafc0f855a3f8a2b34abd73ad7
duncanhs <84508932+duncanhs@users.noreply.github.com>
2022-11-25 15:42:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -882,5 +882,6 @@ def func(n_clicks):
 #app.run_server(debug=True)
 
 if __name__ == '__main__':
-    app.run_server(host = '0.0.0.0', debug=True)
+    app.run()
+    #app.run_server(host = '0.0.0.0', debug=True)
     #serve(server, host='0.0.0.0', port=5000, url_scheme = 'https')
