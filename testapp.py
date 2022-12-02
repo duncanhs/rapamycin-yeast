6f78beba760182cf6a49cc648cf073ea6e448ffa
duncanhs <84508932+duncanhs@users.noreply.github.com>
2022-12-02 15:03:24 +0100

    Update testapp.py

--- testapp.py
+++ testapp.py
@@ -14,7 +14,7 @@ from flask import Flask
 app = Flask(__name__)
 
 from werkzeug.middleware.proxy_fix import ProxyFix
-pu
+
 app.wsgi_app = ProxyFix(
     app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
 )
