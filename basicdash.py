5cd6df7301d16d48a48fea73dc30fd8644a8420e
duncanhs <84508932+duncanhs@users.noreply.github.com>
2022-12-07 11:12:30 +0100

    Update basicdash.py

--- basicdash.py
+++ basicdash.py
@@ -6,7 +6,7 @@ Created on Wed Dec  7 11:09:10 2022
 """
 from dash import Dash
 import dash_html_components as html
-
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Dash(
     __name__,
@@ -14,6 +14,10 @@ app = Dash(
 )
 
 app.layout = html.Div(id='example-div-element')
+
+app.server.wsgi_app = ProxyFix(
+    app.server.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+)
 
 
 if __name__ == '__main__':
