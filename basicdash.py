675e95fc0acdb14cafdf8cb50662c8ff2abe2ec2
duncanhs <84508932+duncanhs@users.noreply.github.com>
2022-12-07 13:44:04 +0100

    Update basicdash.py

--- basicdash.py
+++ basicdash.py
@@ -5,10 +5,10 @@ Created on Wed Dec  7 11:09:10 2022
 @author: hduncan
 """
 
-import dash
-import dash_html_components
+# import dash
+# import dash_html_components
 
-app = dash.Dash(__name__)
+# app = dash.Dash(__name__, url_base_pathname= '/dash')
 app.config.suppress_callback_exceptions = True
 # app.config.update(
 #  	{
@@ -16,12 +16,24 @@ app.config.suppress_callback_exceptions = True
 # 		'requests_pathname_prefix': ''
 #  	}
 # )
-server = app.server
-app.layout = dash_html_components.Div(
- 	[
-		dash_html_components.H1("Hello Dash World")
- 	]
-)
+# server = app.server
+# app.layout = dash_html_components.Div(
+#  	[
+# 		dash_html_components.H1("Hello Dash World")
+#  	]
+# )
 
-if __name__ == '__main__':
-    app.run_server()
+# if __name__ == '__main__':
+#     app.run_server()
+import flask
+import dash
+
+server = flask.Flask(__name__)
+
+app = dash.Dash(__name__,
+               url_base_pathname = '/dash',
+               server = server)
+
+@server.route('/dash')
+def hello():
+    return 'Hello, World!'
