c8359adcb2fed29ee3e6250e650778e2ec002aed
duncanhs <84508932+duncanhs@users.noreply.github.com>
2022-12-07 11:32:36 +0100

    Update testapp.py

--- testapp.py
+++ testapp.py
@@ -13,11 +13,11 @@ from flask import Flask
 # current module (__name__) as argument.
 app = Flask(__name__)
 
-from werkzeug.middleware.proxy_fix import ProxyFix
+# from werkzeug.middleware.proxy_fix import ProxyFix
 
-app.wsgi_app = ProxyFix(
-    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
-)
+# app.wsgi_app = ProxyFix(
+#     app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+# )
 
 # The route() function of the Flask class is a decorator,
 # which tells the application which URL should call
