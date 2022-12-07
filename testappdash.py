2f0334f43077d3ae4903aae792b965255b5e3737
duncanhs <84508932+duncanhs@users.noreply.github.com>
2022-12-07 10:48:15 +0100

    Update testappdash.py

--- testappdash.py
+++ testappdash.py
@@ -14,17 +14,15 @@ from dash import Dash
 # current module (__name__) as argument.
 #app = Flask(__name__)
 
-# app = Dash(__name__,
-#            #server = server,
-#            suppress_callback_exceptions = True
-#            )
-
-app = Dash()
+app = Dash(__name__,
+           #server = server,
+           suppress_callback_exceptions = True
+           )
 
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 app.server.wsgi_app = ProxyFix(
-    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+    app.server.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
 )
 
 server = app.server
